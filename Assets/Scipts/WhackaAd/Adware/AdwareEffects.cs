5f6c899c4d59fb3ca13b1db04fef64fa5eed17b5
Greg <gwwq7157@gmail.com>
2022-07-27 13:36:42 +0800

    started on adware effects

---
+++ Assets/Scipts/WhackaAd/Adware/AdwareEffects.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WhackaAd
+{
+    public class AdwareEffects
+    {
+        public bool Movement;
+        public bool TeleportEffect;
+    }
+}
