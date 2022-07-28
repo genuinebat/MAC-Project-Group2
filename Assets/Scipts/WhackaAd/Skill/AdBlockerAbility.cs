de986baba7813e27eeeca4a57c350999b960506a
Greg <gwwq7157@gmail.com>
2022-07-28 19:49:24 +0800

    Auto stash before merge of "main" and "origin/main"

---
+++ Assets/Scipts/WhackaAd/Skill/AdBlockerAbility.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WhackaAd
+{
+    public class AdBlockerAbility : MonoBehaviour
+    {
+        [Header("Reference Variables")]
+        public GameObject AdBlockderBtn;
+        public GameObject PreAimUI;
+        
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+            
+        }
+
+        public void AdBlockerPressed()
+        {
+            
+        }
+    }
+}
