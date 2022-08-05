a07892a2b52fec75d71cbd789724e6eec6063b98
AwJayen <awjayen12@gmail.com>
2022-08-05 10:14:37 +0800

    Revert "Revert "added scripts and folder for ransomman""
    This reverts commit 87a7e46eee97ce7d8d6e48b8e53c7b7a601bfe39.

---
+++ Assets/Scipts/RansomMan/ByteCollector/PrimaryStorage.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RansomMan
+{
+    public class PrimaryStorage : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            Debug.Log("hi");
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
